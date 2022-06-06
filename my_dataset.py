44f8ef5d415cfc3d97773825e9541ff7e3606797
fly <zxmfly5177@gmail.com>
2022-06-06 14:15:36 +0800

    first commit

--- my_dataset.py
+++ my_dataset.py
@@ -26,7 +26,7 @@ class VOCDataSet(Dataset):
                              for line in read.readlines() if len(line.strip()) > 0]
 
         # read class_indict
-        json_file = "./steel_defect_classes.json"
+        json_file = "./SSD/steel_defect_classes.json"
         assert os.path.exists(json_file), "{} file not exist.".format(json_file)
         with open(json_file, 'r') as f:
             self.class_dict = json.load(f)
